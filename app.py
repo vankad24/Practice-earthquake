d4711f374dad8b03c797d4420e941c2d6b2fb93b
Vankad24 <vankad24@mail.ru>
2023-06-26 20:52:48 +0800

    Delete unnecessary files

--- app.py
+++
@@ -1,8 +0,0 @@
-def print_hello():
-    print("Hello from app.py")
-
-
-a = 5
-b = 19
-print(a+b)
-print_hello()
--- super-mega-test.py
+++
@@ -1,3 +0,0 @@
-import requests
-print(requests.get('https://geek-jokes.sameerkumar.website/api?format=json').json()["joke"])
-print("Hello there!")
