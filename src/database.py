49c46dbc1862630783ace177f6c26cb8f6b88aad
Vankad24 <vankad24@mail.ru>
2023-06-29 16:07:10 +0800

    Fix error if database folder does not exits

--- src/database.py
+++ src/database.py
@@ -1,7 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./static/databases/sql_app.db"
+DATABASE_FOLDER = "./static/databases"
+os.makedirs(DATABASE_FOLDER)
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DATABASE_FOLDER}/sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
